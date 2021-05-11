edfbc460381b8ef8363ee2023152c746cfaafdb6
P1ndi <petrikd@gmail.com>
2021-05-11 17:06:01 +0200

    testovanie gitu

---
+++ cezar.py
@@ -0,0 +1,33 @@
+#cezar mistra sifra
+rot = int(input("zadaj kluc: "))
+action = input("chces [s]ifrovat alebo [d]esifrovat: ")
+data = input("Zadaj text: ")
+
+if action == "s" or action == "sifrovat":
+    text = ""
+    for char in data:
+        char_ord = ord(char)
+        if 32 <= char_ord <= 126:
+            char_ord -= 32
+            char_ord += rot
+            char_ord = char_ord % 94
+            char_ord += 32
+            text += chr(char_ord)
+        else:
+            text += char
+    print("sifra: '{}'".format(text))
+elif action == "d" or action == "desifrovat":
+    text = ""
+    for char in data:
+        char_ord = ord(char)
+        if 32 <= char_ord <= 126:
+            char_ord -= 32
+            char_ord -= rot
+            char_ord = char_ord % 94
+            char_ord += 32
+            text += chr(char_ord)
+        else:
+            text += char
+    print("text: '{}".format(text))
+else:
+    print("Nieco neprebehlo v poriadku")
